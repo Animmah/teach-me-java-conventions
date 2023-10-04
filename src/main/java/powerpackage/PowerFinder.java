0e5f68e069fe7bda96bb0bc6b8a2b22f53e3716f
Aniket Manoj Mahabole <71322677+Animmah@users.noreply.github.com>
2023-10-04 22:42:47 +0530

    Update PowerFinder.java

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -2,12 +2,12 @@ package powerpackage;
 
 public class PowerFinder {
     public static void main(String[] args) {
-        System.out.println(OF(1,1));
-        System.out.println(OF(2,2));
-        System.out.println(OF(3,2));
-        System.out.println(OF(2,1));
+        System.out.println(of(1,1));
+        System.out.println(of(2,2));
+        System.out.println(of(3,2));
+        System.out.println(of(2,1));
     }
-    public static int OF(int x,int y){
+    public static int of(int x,int y){
         int res=1;
         for(int i=0;i<y;i++){
             res*=x;
